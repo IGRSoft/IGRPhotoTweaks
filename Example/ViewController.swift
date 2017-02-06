0c5ad56cea7e853fe8bec42bea0667f176320105
Vitalii Parovishnyk <korich.vi.p@gmail.com>
2017-02-06 10:10:03 +0700

    Added Project

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IGRPhotoTweaks
+//
+//  Created by Vitalii Parovishnyk on 2/6/17.
+//  Copyright © 2017 IGR Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
