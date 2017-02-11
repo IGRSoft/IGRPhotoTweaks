202f48c0c5e996edc3b991cfa794c71dd8606022
Vitalii Parovishnyk <korich.vi.p@gmail.com>
2017-02-12 01:24:46 +0700

    Added test framework

---
+++ IGRPhotoTweaks/IGRPhotoTweaks.h
@@ -0,0 +1,19 @@
+//
+//  IGRPhotoTweaks.h
+//  IGRPhotoTweaks
+//
+//  Created by Vitalii Parovishnyk on 2/12/17.
+//  Copyright © 2017 IGR Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IGRPhotoTweaks.
+FOUNDATION_EXPORT double IGRPhotoTweaksVersionNumber;
+
+//! Project version string for IGRPhotoTweaks.
+FOUNDATION_EXPORT const unsigned char IGRPhotoTweaksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IGRPhotoTweaks/PublicHeader.h>
+
+
